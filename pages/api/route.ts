6505eaf02772f66fe1075a54a10d367cdf79c21c
Alex Rattray <rattray.alex@gmail.com>
2023-09-07 23:13:42 -0400

    Add pages router

---
+++ pages/api/route.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { OpenAI } from "openai";
+
+type ResponseData = {
+  message: string;
+};
+
+// The bug only appears for me when edge is not enabled.
+// export const runtime = "edge";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  const client = new OpenAI();
+  const result = await client.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: "hi",
+      },
+    ],
+    model: "gpt-3.5-turbo",
+  });
+  console.log(result);
+  res.status(200).json({ message: JSON.stringify(result) });
+}
