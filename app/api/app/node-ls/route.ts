498294a47e1a6718ff13e63908a8f3bd8dc4924c
Andy Edwards <jedwards@fastmail.com>
2023-09-08 14:53:31 -0500

    import openai in try/catch in ls routes

--- app/api/app/node-ls/route.ts
+++ app/api/app/node-ls/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { listOpenAIPackageDir } from "@/listOpenAIPackageDir";
 
 export async function GET() {
+  await import("openai").catch(() => {});
   const files = await listOpenAIPackageDir();
   return new NextResponse(files.join("\n") + "\n");
 }
--- pages/api/pages/node-ls.ts
+++ pages/api/pages/node-ls.ts
@@ -5,6 +5,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  await import("openai").catch(() => {});
   const files = await listOpenAIPackageDir();
   res.status(200).end(files.join("\n") + "\n");
 }
